61b6f6026c8622a8adde1d53f4b6119ef494c13b
曹鑫 <caoxin@caoxindeMacBook-Pro.local>
2023-05-18 20:10:17 +0800

    第一次提交

---
+++ src/main/java/com/github/xincao9/redisexperiment/RedisExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.github.xincao9.redisexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/xincao9/redisexperiment/RedisExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xincao9.redisexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
